e6bfd75f249e85e45b67025612c5ed8325b60e2c
novoa.lopez@gmail.com <novoa.lopez@gmail.com>
2023-05-22 14:20:33 -0300

    Adiciona model Pet

---
+++ ExecuteService/Models/PetStore/Controllers/Pet.cs
@@ -0,0 +1,25 @@
+﻿namespace ExecuteService.Models.PetStore.Controllers
+{
+    public class PetRequest
+    {
+        public int id { get; set; }
+        public Category category { get; set; }
+        public string name { get; set; }
+        public string[] photoUrls { get; set; }
+        public Tag[] tags { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Tag
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+}
