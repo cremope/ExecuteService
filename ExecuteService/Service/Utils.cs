fdbc427ab6deb04b287e1cef67e7f58a80842583
novoa.lopez@gmail.com <novoa.lopez@gmail.com>
2023-05-29 16:00:40 -0300

    Adiciona classe Utils para centralizar metodos de execucao dos endpoitns

---
+++ ExecuteService/Service/Utils.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace ExecuteService.Service
+{
+    public static class Utils
+    {
+        private static RestClient client;
+        private static RestRequest endpoint;
+        private static RestResponse response;
+        public static RestClient Client(string urlBase)
+        {
+            client = new RestClient(urlBase);
+            return client;
+        }
+
+        public static RestRequest Endpoint(string path)
+        {
+            endpoint = new RestRequest(path);
+            return endpoint;
+        }
+
+        public static void Header_ApiKey()
+        {
+            endpoint.AddParameter("api_key", "special-key", ParameterType.HttpHeader);
+        }
+
+        public static void Method(Method method)
+        {
+            endpoint.Method = method;
+        }
+
+        public static void Request(object body)
+        {
+            endpoint.AddParameter("application/json", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+        }
+
+        public static RestResponse Execute()
+        {
+            response = client.Execute(endpoint);
+            return response;
+        }
+    }
+}
